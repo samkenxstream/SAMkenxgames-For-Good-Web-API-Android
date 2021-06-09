7e39e016eb68e5303f78971cda27c07025a1d0d7
mvityk <mvityk@mindgrub.com>
2021-06-09 15:32:50 -0400

    Initial commit

---
+++ app/src/main/java/com/example/games_for_good_web_api_android/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.games_for_good_web_api_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
